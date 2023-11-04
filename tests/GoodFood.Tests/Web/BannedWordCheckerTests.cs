2bfdcd0b10fcb7ae2cb5f0710a7332a3e946bc6c
Hakim Ghods <ghods.hakim@hotmail.com>
2023-11-04 12:33:03 +0330

    Testing with Moq

--- tests/GoodFood.Tests/Web/BannedWordCheckerTests.cs
+++ tests/GoodFood.Tests/Web/BannedWordCheckerTests.cs
@@ -7,33 +7,42 @@ using System.Threading.Tasks;
 using GoodFood.Web.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.FileProviders;
+using Moq;
 
 namespace GoodFood.Tests.Web;
 
-public class MyWebHostEnviroment : IWebHostEnvironment
-{
-    public string WebRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public IFileProvider WebRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public string ContentRootPath
-    {
-        get
-        {
-            var location = typeof(BannedWordCheckerTests).GetTypeInfo().Assembly.Location;
-            return Path.GetDirectoryName(location);
-        }
-        set => throw new NotImplementedException();
-    }
-    public string EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-}
+//stub
+//public class MyWebHostEnviroment : IWebHostEnvironment
+//{
+//    public string WebRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+//    public IFileProvider WebRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+//    public string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+//    public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+//    public string ContentRootPath
+//    {
+//        get
+//        {
+//            var location = typeof(BannedWordCheckerTests).GetTypeInfo().Assembly.Location;
+//            return Path.GetDirectoryName(location);
+//        }
+//        set => throw new NotImplementedException();
+//    }
+//    public string EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+//}
 public class BannedWordCheckerTests
 {
     [Fact]
     public async Task Input_Contains_Banned_Word_returns_true()
     {
         //Arrange
-        var sut = new BannedWordChecker(new MyWebHostEnviroment());
+        var webHostMock = new Mock<IWebHostEnvironment>();
+
+        var location = typeof(BannedWordCheckerTests).GetTypeInfo().Assembly.Location;
+        var path= Path.GetDirectoryName(location);
+
+        webHostMock.Setup(w => w.ContentRootPath).Returns(path);
+
+        var sut = new BannedWordChecker(webHostMock.Object);
 
         //Act
         var result = await sut.CheckForBannedWordAsync("nazi");
