3e3919f8ee95d968aee367c6f15bf50a3286c509
Hakim Ghods <ghods.hakim@hotmail.com>
2023-11-17 10:37:23 +0330

    tests

--- tests/GoodFood.Tests/Web/FoodImagePathServiceTests.cs
+++ tests/GoodFood.Tests/Web/FoodImagePathServiceTests.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using GoodFood.Web.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting.Internal;
 using Moq;
 
 namespace GoodFood.Tests.Web;
@@ -25,5 +26,29 @@ public class FoodImagePathServiceTests
 
         // Assert
         Assert.EndsWith(Path.Combine("Files", "FoodImages"), result, StringComparison.Ordinal);
+    }
+
+    public class FoodImagePathServiceIntegrationTests
+    {
+        [Fact]
+        public void GetPath_ShouldReturnCorrectPathInIntegrationTest()
+        {
+            // Arrange
+            var contentRootPath = Path.Combine(Directory.GetCurrentDirectory(), "TestContentRoot");
+            var environment = new HostingEnvironment
+            {
+                ContentRootPath = contentRootPath,
+                ContentRootFileProvider = new PhysicalFileProvider(contentRootPath)
+            };
+
+            var service = new FoodImagePathService(environment);
+
+            // Act
+            var result = service.GetPath();
+
+            // Assert
+            var expectedPath = Path.Combine(contentRootPath, "Files", "FoodImages");
+            Assert.Equal(expectedPath, result);
+        }
     }
 }
